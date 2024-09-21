95f33ae8e4a921a9b55e56c0ee11ecee2c0986d7
siararisa-iac <126797704+siararisa-iac@users.noreply.github.com>
2024-09-21 10:01:19 +0800

    Added BehaviorTree node scripts

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -7,8 +7,20 @@ public class Inventory : MonoBehaviour
     [SerializeField]
     private List<string> items = new List<string>();
 
+    private void Awake()
+    {
+        ActionNode actionNode = new(CheckInventoryForMeat);
+    }
     public bool ContainsItem(string id)
     {
         return (items.Contains(id));
+    }
+
+    public NodeState CheckInventoryForMeat()
+    {
+        if (items.Contains("Meat"))
+            return NodeState.SUCCESS;
+        else
+            return NodeState.FAILURE;
     }
 }
